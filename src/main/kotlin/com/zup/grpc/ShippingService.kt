07f44e66581fc86eaf30e3e7990cba2ac6c17141
Afonso Mateus <afonsoprad10@gmail.com>
2021-05-12 07:51:21 -0400

    add validation to fields

--- src/main/kotlin/com/zup/grpc/ShippingService.kt
+++ src/main/kotlin/com/zup/grpc/ShippingService.kt
@@ -3,6 +3,7 @@ package com.zup.grpc
 import com.zup.grpc.frete.ShippingRequest
 import com.zup.grpc.frete.ShippingResponse
 import com.zup.grpc.frete.ShippingServiceGrpc
+import io.grpc.Status
 import io.grpc.stub.StreamObserver
 import javax.inject.Singleton
 import kotlin.random.Random
@@ -14,8 +15,36 @@ class ShippingService : ShippingServiceGrpc.ShippingServiceImplBase() {
 		request: ShippingRequest?,
 		responseObserver: StreamObserver<ShippingResponse>?
 	) {
+		val cep = request!!.cep
+
+		if (cep == null || cep.isBlank()) {
+			val error = Status.INVALID_ARGUMENT
+					.withDescription("cep cannot be null or blank")
+					.asRuntimeException()
+			responseObserver?.onError(error)
+			return
+		}
+		if (!cep.matches("[0-9]{5}-[\\d]{3}".toRegex())) {
+
+			val error = Status.INVALID_ARGUMENT
+					.withDescription("invalid cep format")
+					.augmentDescription("provide a cep in format 99999-999")
+					.asRuntimeException()
+			responseObserver?.onError(error)
+			return
+		}
+		if (cep.endsWith("333")) {
+
+			val error = Status.PERMISSION_DENIED
+					.withDescription("Access denied")
+					.augmentDescription("token expired")
+					.asRuntimeException()
+			responseObserver?.onError(error)
+			return
+		}
+
 		val response = ShippingResponse.newBuilder()
-				.setCep(request?.cep)
+				.setCep(cep)
 				.setValue(Random.nextDouble(10.0, 200.0))
 				.build()
 
